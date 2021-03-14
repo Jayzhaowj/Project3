1a2357cacaecf883f64e40b6b1a462ff9f5a16a4
Wenjie Zhao <wjzhao@ucsc.edu>
2021-03-14 14:29:25 -0700

    update Rscripts

--- hierarchical/walking_EEG/Rscripts/fit_data.R
+++ hierarchical/walking_EEG/Rscripts/fit_data.R
@@ -19,5 +19,6 @@ sample_size <- 1000
 ####### construct discount factor #########
 delta <- seq(0.995, 0.999, by = 0.001)
 delta_matrix <- as.matrix(expand.grid(delta, delta))
+ptm
 result <- hparcor(yt=EEG_data$stand_pull$data[1, , ], P=P, delta = delta_matrix, 
                   sample_size=sample_size, chains=1, DIC = TRUE, uncertainty = FALSE)
