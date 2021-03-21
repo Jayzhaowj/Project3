596c42b581bae4e8a0a3f0a9a927469a2de27b3f
Wenjie Zhao <wjzhao@ucsc.edu>
2021-03-21 16:17:08 -0700

    add results from subjects 25 with discount factor from 0.9 to 0.999

--- hierarchical/walking_EEG/Rscripts/plot_summary.R
+++ hierarchical/walking_EEG/Rscripts/plot_summary.R
@@ -33,7 +33,7 @@ library(snowfall)
 root_dir <- getwd()
 
 ######## subject id #######
-subject_id <- 6
+subject_id <- 25
 load(file = paste0(root_dir, "/data/S", subject_id, ".RData"))
 
 
@@ -42,14 +42,17 @@ cond_type <- "walk_rotate"
 cluster_area <- EEG_data[[cond_type]]$names
 times <- EEG_data[[cond_type]]$times
 raw_data <- EEG_data[[cond_type]]$data
+data <- raw_data
 
 ### do the first difference
-data_diff <- apply(raw_data, c(1,3), diff)
-data <- aperm(data_diff, c(2,1,3))
+# data_diff <- apply(raw_data, c(1,3), diff)
+# data <- aperm(data_diff, c(2,1,3))
+# diff_times <- as.numeric(times[1, 1:n_t+1])
 
+#### number of time series
 n_t <- dim(data)[2]
 P <- 10
-diff_times <- as.numeric(times[1, 1:n_t+1])
+
 
 ######### load results ############
 load(file = paste0(root_dir, "/results/", cond_type, "_S", subject_id, ".RData"))
@@ -99,6 +102,55 @@ for(index in 1:dim(data)[1]){
   dev.off()
 }
 
+
+
+#### draw ERSPs
+######### range of zlim ##########
+zlim <- c(-0.5, .5)
+ylim <- c(4, 50)
+xlim <- c(-.5, 1.5)
+cex <- 2
+for(index in 1:dim(data)[1]){
+  main <- cluster_area[index]
+  s_mean_diff <- t(apply(s_mean[[index]][1, , ], 1, 
+                       function(x) x - s_mean[[index]][1, which(times==-500), ]))
+  png(filename = paste0(plot_dir, '/est_diff_mean_ch_', cluster_area[index], '.png'))
+  par(cex.lab = cex, cex.axis = cex, cex.main = cex)
+  draw_density_hier_wEEG(w = w, P = P, times = times, cond_type = cond_type,
+                         n_t = n_t, s = s_mean_diff, 
+                         main = main,
+                         zlim = zlim, 
+                         ylim = ylim,
+                         xlim = xlim)
+  dev.off()
+  
+  s_mean_quantile_diff <- t(apply(s_mean_quantile[[index]][1, , ], 1, 
+                                function(x) x - s_mean_quantile[[index]][1, which(times==-500), ]))
+  png(filename = paste0(plot_dir, '/est_diff_lb_ch_', cluster_area[index], '.png'))
+  par(cex.lab = cex, cex.axis = cex, cex.main = cex)
+  draw_density_hier_wEEG(w = w, P = P, times = times, cond_type = cond_type,
+                         n_t = n_t, s = s_mean_quantile_diff,
+                         main = main,
+                         zlim = zlim, 
+                         ylim = ylim,
+                         xlim = xlim)
+  dev.off()
+  
+  s_mean_quantile_diff <- t(apply(s_mean_quantile[[index]][2, , ], 1, 
+                                  function(x) x - s_mean_quantile[[index]][2, which(times==-500), ]))
+  png(filename = paste0(plot_dir, '/est_diff_ub_ch_', cluster_area[index], '.png'))
+  par(cex.lab = cex, cex.axis = cex, cex.main = cex)
+  draw_density_hier_wEEG(w = w,  P = P, times=times, cond_type = cond_type,
+                         n_t = n_t, s = s_mean_quantile_diff,
+                         main = main,
+                         zlim = zlim, 
+                         ylim = ylim,
+                         xlim = xlim)
+  dev.off()
+}
+
+
+
 ###########################################################################################
 
 ####### name of cluster area #######
@@ -106,14 +158,16 @@ cond_type <- "walk_pull"
 cluster_area <- EEG_data[[cond_type]]$names
 times <- EEG_data[[cond_type]]$times
 raw_data <- EEG_data[[cond_type]]$data
+data <- raw_data
 
 ### do the first difference
-data_diff <- apply(raw_data, c(1,3), diff)
-data <- aperm(data_diff, c(2,1,3))
+# data_diff <- apply(raw_data, c(1,3), diff)
+# data <- aperm(data_diff, c(2,1,3))
+# diff_times <- as.numeric(times[1, 1:n_t+1])
 
 n_t <- dim(data)[2]
 P <- 10
-diff_times <- as.numeric(times[1, 1:n_t+1])
+
 
 ######### load results ############
 load(file = paste0(root_dir, "/results/", cond_type, "_S", subject_id, ".RData"))
@@ -166,14 +220,16 @@ cond_type <- "stand_rotate"
 cluster_area <- EEG_data[[cond_type]]$names
 times <- EEG_data[[cond_type]]$times
 raw_data <- EEG_data[[cond_type]]$data
+data <- raw_data
 
-### do the first difference
-data_diff <- apply(raw_data, c(1,3), diff)
-data <- aperm(data_diff, c(2,1,3))
+# ### do the first difference
+# data_diff <- apply(raw_data, c(1,3), diff)
+# data <- aperm(data_diff, c(2,1,3))
+# diff_times <- as.numeric(times[1, 1:n_t+1])
 
 n_t <- dim(data)[2]
 P <- 10
-diff_times <- as.numeric(times[1, 1:n_t+1])
+
 
 ######### load results ############
 load(file = paste0(root_dir, "/results/", cond_type, "_S", subject_id, ".RData"))
@@ -229,14 +285,16 @@ cond_type <- "stand_pull"
 cluster_area <- EEG_data[[cond_type]]$names
 times <- EEG_data[[cond_type]]$times
 raw_data <- EEG_data[[cond_type]]$data
+data <- raw_data
 
-### do the first difference
-data_diff <- apply(raw_data, c(1,3), diff)
-data <- aperm(data_diff, c(2,1,3))
+# ### do the first difference
+# data_diff <- apply(raw_data, c(1,3), diff)
+# data <- aperm(data_diff, c(2,1,3))
+# diff_times <- as.numeric(times[1, 1:n_t+1])
 
 n_t <- dim(data)[2]
 P <- 10
-diff_times <- as.numeric(times[1, 1:n_t+1])
+
 
 ######### load results ############
 load(file = paste0(root_dir, "/results/", cond_type, "_S", subject_id, ".RData"))
